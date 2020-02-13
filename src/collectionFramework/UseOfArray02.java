9f276dc95f90075c3358feb393c8d5e4bc0d14a3
tikly <ttaekymm@gmail.com>
2020-02-12 22:45:19 -0500

    My titly's commit

---
+++ src/collectionFramework/UseOfArray02.java
@@ -0,0 +1,28 @@
+package collectionFramework;
+
+public class UseOfArray02 {
+
+	public static void main(String[] args) {
+		int []a=new int [7];
+		a[0]=4;
+		a[1]=8;
+		a[2]=12;
+		a[3]=16;
+		a[4]=20;
+		a[4]=25;
+	System.out.println("The printing value of 1st is :"+a[0]);
+	System.out.println("The printing value of 2nd is :"+a[1]);
+	System.out.println("The printing value of 3rd is :"+a[2]);
+	System.out.println("The printing value of 4th is :"+a[3]);
+	System.out.println("The printing value of 5th is :"+a[4]);
+	System.out.println("The printing value of 6th is :"+a[5]);
+	System.out.println("The size of the the array is  :"+a.length);
+	// print all index value:
+	System.out.println("Printing all index value of Array a");
+	for(int i=0;i<a.length;i++) {
+		System.out.println(a[i]);//ask tofael vi 
+	}
+	}
+	
+
+}
